cad7cd5b1cb8ced7a329c63ae24a776f8ae2ff9f
Бадый Шагаалан <badyjsagaalan@Badyjs-MacBook-Pro.local>
2021-03-28 12:40:17 -0700

    commit

---
+++ YandexSchoolTestTask/Cache.swift
@@ -0,0 +1,8 @@
+//
+//  Cache.swift
+//  YandexSchoolTestTask
+//
+//  Created by Бадый Шагаалан on 27.03.2021.
+//
+
+import Foundation
---
+++ YandexSchoolTestTask/Network/WSResponse.swift
@@ -0,0 +1,8 @@
+//
+//  WSResponse.swift
+//  YandexSchoolTestTask
+//
+//  Created by Бадый Шагаалан on 27.03.2021.
+//
+
+import Foundation
